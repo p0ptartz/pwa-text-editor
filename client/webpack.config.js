fc74f161a2608726f221b2f0eeb8be9d6973c0dc
p0ptartz <ericwm420@gmail.com>
2023-03-09 15:06:21 -0500

    did stuff

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,13 +1,7 @@
-
-
-
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const WebpackPwaManifest = require('webpack-pwa-manifest');
 const path = require('path');
 const { InjectManifest } = require('workbox-webpack-plugin');
-
-// TODO: Add and configure workbox plugins for a service worker and manifest file.
-// TODO: Add CSS loaders and babel to webpack.
 
 module.exports = () => {
   return {
@@ -20,27 +14,27 @@ module.exports = () => {
       filename: '[name].bundle.js',
       path: path.resolve(__dirname, 'dist'),
     },
-    plugins: [// Webpack plugin that generates our html file and injects our bundles. 
+    optimization: {
+      splitChunks: {
+        chunks: 'all',
+      },
+    },
+    plugins: [
       new HtmlWebpackPlugin({
         template: './index.html',
         title: 'Text editor'
       }),
-
-      // Injects our custom service worker
       new InjectManifest({
         swSrc: './src-sw.js',
         swDest: 'src-sw.js',
       }),
-
-      // Creates a manifest.json file.
       new WebpackPwaManifest()
     ],
     module: {
       rules: [
         {
           test: /\.css$/,
-          use: [
-            'style-loader', 'css-loader'],
+          use: ['style-loader', 'css-loader'],
         },
         {
           test: /\.m?js$/,
